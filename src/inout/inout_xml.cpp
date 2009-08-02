5990c2022054d533f9c4f15fb726f2a12fd6c227
ulrichard <ulrichard@6cb3a368-a3d1-4211-a919-f85a3bdf5603>
2009-08-02 20:46:37 +0000

    openin streams for xml io in binary mode to hopefully prevent a stream error that happens on voyage but not on ubuntu.

--- src/inout/inout_xml.cpp
+++ src/inout/inout_xml.cpp
@@ -18,7 +18,7 @@ FlightDatabase inout_xml::read(const bfs::path &source)
 {
 	FlightDatabase fldb("");
 	// create and open an archive for input
-    bfs::ifstream ifs(source);
+    bfs::ifstream ifs(source, std::ios_base::in | std::ios_base::binary);
 	if(!ifs.good())
         throw std::runtime_error("file not found");
 	boost::archive::xml_iarchive ia(ifs);
@@ -31,7 +31,7 @@ FlightDatabase inout_xml::read(const bfs::path &source)
 void inout_xml::write(const FlightDatabase &fdb, const bfs::path &destination)
 {
     bfs::create_directories(destination.branch_path());
-	bfs::ofstream ofs(destination);
+	bfs::ofstream ofs(destination, std::ios_base::out | std::ios_base::binary | std::ios_base::trunc);
 	if(!ofs.good())
         throw std::runtime_error("could not write to " + destination.string());
 	boost::archive::xml_oarchive oa(ofs);
