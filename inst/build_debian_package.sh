7326a29619e72e3cff3dcbfc1e429d497f85da42
ulrichard <ulrichard@6cb3a368-a3d1-4211-a919-f85a3bdf5603>
2010-07-30 20:47:07 +0000

    changed distro release

--- inst/build_debian_package.sh
+++ inst/build_debian_package.sh
@@ -1,12 +1,11 @@
-dpkg-buildpackage -rfakeroot
-
-
 rm -r builddeb
 mkdir builddeb
 cd builddeb
-svn export https://flugbuch2.svn.sourceforge.net/svnroot/flugbuch2/trunk flugbuch2-2.0.8
-cd flugbuch2-2.0.8
+svn export https://flugbuch2.svn.sourceforge.net/svnroot/flugbuch2/trunk flugbuch2-2.0.9
+cd flugbuch2-2.0.9/inst
 dpkg-buildpackage -rfakeroot -S
 cd ..
-rm -r flugbuch2-2.0.8
-dput ppa:richi-paraeasy/ppa ./flugbuch2_2.0.8_source.changes
+dput ppa:richi-paraeasy/ppa ./flugbuch2_2.0.9_source.changes
+cd ..
+rm -r flugbuch2-2.0.9
+
