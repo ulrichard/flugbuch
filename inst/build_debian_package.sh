ab4cbdaf6e1e56fddc36f6c70957eb72fe8dad16
ulrichard <ulrichard@6cb3a368-a3d1-4211-a919-f85a3bdf5603>
2010-08-01 12:07:54 +0000

    Moved the debian directory from inst/ to the project root. The source tarball resulting from "dpkg-buildpackage -rfakeroot -S" contained only the inst subdir.

--- inst/build_debian_package.sh
+++ inst/build_debian_package.sh
@@ -1,11 +1,10 @@
 rm -r builddeb
 mkdir builddeb
 cd builddeb
-svn export https://flugbuch2.svn.sourceforge.net/svnroot/flugbuch2/trunk flugbuch2-2.0.10
-cd flugbuch2-2.0.10/inst
+svn export https://flugbuch2.svn.sourceforge.net/svnroot/flugbuch2/trunk flugbuch2-2.0.11
+cd flugbuch2-2.0.11
 dpkg-buildpackage -rfakeroot -S
 cd ..
-dput ppa:richi-paraeasy/ppa ./flugbuch2_2.0.10_source.changes
-cd ..
-rm -r flugbuch2-2.0.10
+rm -r flugbuch2-2.0.11
+dput ppa:richi-paraeasy/ppa ./flugbuch2_2.0.11_source.changes
 
