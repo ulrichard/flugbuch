75105b134cc13854073e274773c20985f0bb7e28
ulrichard <ulrichard@6cb3a368-a3d1-4211-a919-f85a3bdf5603>
2010-08-04 23:19:32 +0000

    fixed the hanging debian/postinst that troubled a while. I found the solution in the postinst script of the gallery package of which I found a mailing list that indicated that they used to have the same problem.

--- inst/build_debian_package.sh
+++ inst/build_debian_package.sh
@@ -1,10 +1,10 @@
 rm -r builddeb
 mkdir builddeb
 cd builddeb
-svn export https://flugbuch2.svn.sourceforge.net/svnroot/flugbuch2/trunk flugbuch2-2.0.13
-cd flugbuch2-2.0.13
+svn export https://flugbuch2.svn.sourceforge.net/svnroot/flugbuch2/trunk flugbuch2-2.0.14
+cd flugbuch2-2.0.14
 dpkg-buildpackage -rfakeroot -S
 cd ..
-rm -r flugbuch2-2.0.13
-dput ppa:richi-paraeasy/ppa ./flugbuch2_2.0.13_source.changes
+rm -r flugbuch2-2.0.14
+dput ppa:richi-paraeasy/ppa ./flugbuch2_2.0.14_source.changes
 
